abfd9d6c14673efdfc641b75c6e59b1341044f57
Fabian <49373262+fabian1409@users.noreply.github.com>
2026-01-20 16:04:39 +0100

    feat: improve oprf-service WS error responses [TAC-112] (#380)
    * feat: improve oprf-service WS error responses
    
    * refactor: added error from impl for serde/cbor
    
    ---------
    
    Co-authored-by: 0xThemis <118167989+0xThemis@users.noreply.github.com>

--- oprf-service/src/api/v1.rs
+++ oprf-service/src/api/v1.rs
@@ -313,13 +313,10 @@ async fn read_request<Msg: for<'de> Deserialize<'de>>(
 ) -> Result<(Msg, HumanReadable), Error> {
     let res = match socket.recv().await.ok_or(Error::ConnectionClosed)?? {
         ws::Message::Text(json) => (
-            serde_json::from_slice::<Msg>(json.as_bytes()).map_err(|_| Error::UnexpectedMessage)?,
+            serde_json::from_slice::<Msg>(json.as_bytes())?,
             HumanReadable::Yes,
         ),
-        ws::Message::Binary(cbor) => (
-            ciborium::from_reader(cbor.as_ref()).map_err(|_| Error::UnexpectedMessage)?,
-            HumanReadable::No,
-        ),
+        ws::Message::Binary(cbor) => (ciborium::from_reader(cbor.as_ref())?, HumanReadable::No),
         ws::Message::Close(_) => return Err(Error::ConnectionClosed),
         _ => return Err(Error::UnexpectedMessage),
     };
