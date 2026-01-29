9be468a2a7dc68f510aac508ac92daf4d694ca6f
Daniel Kales <11509575+dkales@users.noreply.github.com>
2026-01-29 10:24:52 +0100

    Revert "feat: move TestSecretManager trait impls to oprf-test-utils again (#407)"
    This reverts commit 76ad208206bd6594b4361e956e2835740595334c.

--- oprf-test-utils/src/test_secret_manager.rs
+++ oprf-test-utils/src/test_secret_manager.rs
@@ -7,10 +7,8 @@ use std::{
 
 use alloy::{primitives::U160, signers::local::PrivateKeySigner};
 use ark_ff::UniformRand;
-use async_trait::async_trait;
 use itertools::Itertools;
 use oprf_core::ddlog_equality::shamir::DLogShareShamir;
-use oprf_service::oprf_key_material_store::OprfKeyMaterialStore;
 use oprf_types::{
     OprfKeyId, ShareEpoch,
     crypto::{OprfKeyMaterial, OprfPublicKey},
@@ -106,85 +104,5 @@ impl TestSecretManager {
 
     pub fn load_key_ids(&self) -> Vec<OprfKeyId> {
         self.store.lock().keys().copied().collect_vec()
-    }
-}
-
-#[async_trait]
-impl oprf_key_gen::secret_manager::SecretManager for TestSecretManager {
-    async fn load_or_insert_wallet_private_key(&self) -> eyre::Result<PrivateKeySigner> {
-        Ok(self
-            .wallet_private_key
-            .clone()
-            .expect("Cannot provide private key for this test run"))
-    }
-
-    async fn get_previous_share(
-        &self,
-        oprf_key_id: OprfKeyId,
-        generated_epoch: ShareEpoch,
-    ) -> eyre::Result<Option<DLogShareShamir>> {
-        let store = self.store.lock();
-        if let Some(oprf_key_material) = store.get(&oprf_key_id)
-            && let Some((stored_epoch, share)) = oprf_key_material.get_latest_share()
-        {
-            tracing::debug!("my latest epoch is: {stored_epoch}");
-            if stored_epoch.next() == generated_epoch {
-                Ok(Some(share))
-            } else {
-                tracing::debug!("we missed an epoch - returning None");
-                Ok(None)
-            }
-        } else {
-            Ok(None)
-        }
-    }
-
-    async fn remove_oprf_key_material(&self, rp_id: OprfKeyId) -> eyre::Result<()> {
-        if self.store.lock().remove(&rp_id).is_none() {
-            panic!("trying to remove oprf_key_id that does not exist");
-        }
-        Ok(())
-    }
-
-    async fn store_dlog_share(
-        &self,
-        oprf_key_id: OprfKeyId,
-        public_key: OprfPublicKey,
-        epoch: ShareEpoch,
-        share: DLogShareShamir,
-    ) -> eyre::Result<()> {
-        let mut store = self.store.lock();
-        if epoch.is_initial_epoch() || !store.contains_key(&oprf_key_id) {
-            assert!(
-                store
-                    .insert(
-                        oprf_key_id,
-                        OprfKeyMaterial::new(BTreeMap::from([(epoch, share)]), public_key,)
-                    )
-                    .is_none(),
-                "On initial epoch, secret-manager must be empty"
-            )
-        } else {
-            store
-                .get_mut(&oprf_key_id)
-                .expect("Checked above")
-                .insert_share(epoch, share);
-        }
-        Ok(())
-    }
-}
-
-#[async_trait]
-impl oprf_service::secret_manager::SecretManager for TestSecretManager {
-    async fn load_secrets(&self) -> eyre::Result<OprfKeyMaterialStore> {
-        Ok(OprfKeyMaterialStore::new(self.store.lock().clone()))
-    }
-
-    async fn get_oprf_key_material(&self, oprf_key_id: OprfKeyId) -> eyre::Result<OprfKeyMaterial> {
-        self.store
-            .lock()
-            .get(&oprf_key_id)
-            .cloned()
-            .ok_or_else(|| eyre::eyre!("oprf_key_id {oprf_key_id} not found"))
     }
 }
