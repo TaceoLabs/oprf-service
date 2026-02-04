91af688626d3cc11b2160fd865ac155c78d7fe1c
0xThemis <118167989+0xThemis@users.noreply.github.com>
2026-02-04 09:33:24 +0100

    test: added macro for key-gen/node-secret-manager in test_utils

--- oprf-test-utils/src/secret_manager/test_secret_manager.rs
+++ oprf-test-utils/src/secret_manager/test_secret_manager.rs
@@ -1,6 +1,9 @@
 use std::{collections::HashMap, str::FromStr, sync::Arc, time::Duration};
 
-use alloy::{primitives::U160, signers::local::PrivateKeySigner};
+use alloy::{
+    primitives::{Address, U160},
+    signers::local::PrivateKeySigner,
+};
 use ark_ff::UniformRand;
 use itertools::Itertools;
 use oprf_core::ddlog_equality::shamir::DLogShareShamir;
@@ -12,6 +15,101 @@ use parking_lot::Mutex;
 use rand::{CryptoRng, Rng};
 
 use crate::TEST_TIMEOUT;
+
+#[macro_export]
+macro_rules! key_gen_test_secret_manager {
+    ($trait: path, $name: ident) => {
+        mod impl_secret_manager {
+            use std::sync::Arc;
+
+            use alloy::signers::local::PrivateKeySigner;
+            use async_trait::async_trait;
+            use oprf_core::ddlog_equality::shamir::DLogShareShamir;
+            use oprf_test_utils::test_secret_manager::TestSecretManager;
+            use oprf_types::{
+                OprfKeyId, ShareEpoch,
+                crypto::{OprfKeyMaterial, OprfPublicKey},
+            };
+            // need a new type to implement the trait
+            pub struct $name(pub Arc<TestSecretManager>);
+            #[async_trait]
+            impl $trait for $name {
+                async fn load_or_insert_wallet_private_key(
+                    &self,
+                ) -> eyre::Result<PrivateKeySigner> {
+                    self.0.load_or_insert_wallet_private_key().await
+                }
+
+                async fn get_share_by_epoch(
+                    &self,
+                    oprf_key_id: OprfKeyId,
+                    generated_epoch: ShareEpoch,
+                ) -> eyre::Result<Option<DLogShareShamir>> {
+                    self.0
+                        .get_share_by_epoch(oprf_key_id, generated_epoch)
+                        .await
+                }
+
+                async fn remove_oprf_key_material(&self, rp_id: OprfKeyId) -> eyre::Result<()> {
+                    self.0.remove_oprf_key_material(rp_id).await
+                }
+
+                async fn store_dlog_share(
+                    &self,
+                    oprf_key_id: OprfKeyId,
+                    public_key: OprfPublicKey,
+                    epoch: ShareEpoch,
+                    share: DLogShareShamir,
+                ) -> eyre::Result<()> {
+                    self.0
+                        .store_dlog_share(oprf_key_id, public_key, epoch, share)
+                        .await
+                }
+            }
+        }
+        use impl_secret_manager::$name;
+    };
+}
+
+#[macro_export]
+macro_rules! oprf_node_test_secret_manager {
+    ($trait: path, $name: ident) => {
+        mod impl_secret_manager {
+            use std::sync::Arc;
+
+            use alloy::primitives::Address;
+            use alloy::signers::local::PrivateKeySigner;
+            use async_trait::async_trait;
+            use oprf_core::ddlog_equality::shamir::DLogShareShamir;
+            use oprf_test_utils::test_secret_manager::TestSecretManager;
+            use oprf_types::{
+                OprfKeyId, ShareEpoch,
+                crypto::{OprfKeyMaterial, OprfPublicKey},
+            };
+            use taceo_oprf_service::oprf_key_material_store::OprfKeyMaterialStore;
+            // need a new type to implement the trait
+            pub struct $name(pub Arc<TestSecretManager>);
+            #[async_trait]
+            impl $trait for $name {
+                async fn load_address(&self) -> eyre::Result<Address> {
+                    self.0.load_address().await
+                }
+                async fn load_secrets(&self) -> eyre::Result<OprfKeyMaterialStore> {
+                    Ok(OprfKeyMaterialStore::new(self.0.store.lock().clone()))
+                }
+
+                async fn get_oprf_key_material(
+                    &self,
+                    oprf_key_id: OprfKeyId,
+                    epoch: ShareEpoch,
+                ) -> eyre::Result<Option<OprfKeyMaterial>> {
+                    self.0.get_oprf_key_material(oprf_key_id, epoch).await
+                }
+            }
+        }
+        use impl_secret_manager::$name;
+    };
+}
 
 #[derive(Clone)]
 pub struct TestSecretManager {
@@ -93,5 +191,71 @@ impl TestSecretManager {
 
     pub fn load_key_ids(&self) -> Vec<OprfKeyId> {
         self.store.lock().keys().copied().collect_vec()
+    }
+
+    pub async fn load_or_insert_wallet_private_key(&self) -> eyre::Result<PrivateKeySigner> {
+        Ok(self.wallet_private_key.clone())
+    }
+
+    pub async fn get_share_by_epoch(
+        &self,
+        oprf_key_id: OprfKeyId,
+        generated_epoch: ShareEpoch,
+    ) -> eyre::Result<Option<DLogShareShamir>> {
+        let store = self.store.lock();
+        if let Some(oprf_key_material) = store.get(&oprf_key_id)
+            && oprf_key_material.is_epoch(generated_epoch)
+        {
+            Ok(Some(oprf_key_material.share()))
+        } else {
+            Ok(None)
+        }
+    }
+
+    pub async fn remove_oprf_key_material(&self, rp_id: OprfKeyId) -> eyre::Result<()> {
+        if self.store.lock().remove(&rp_id).is_none() {
+            panic!("trying to remove oprf_key_id that does not exist");
+        }
+        Ok(())
+    }
+
+    pub async fn store_dlog_share(
+        &self,
+        oprf_key_id: OprfKeyId,
+        public_key: OprfPublicKey,
+        epoch: ShareEpoch,
+        share: DLogShareShamir,
+    ) -> eyre::Result<()> {
+        let mut store = self.store.lock();
+        if epoch.is_initial_epoch() || !store.contains_key(&oprf_key_id) {
+            assert!(
+                store
+                    .insert(oprf_key_id, OprfKeyMaterial::new(share, public_key, epoch))
+                    .is_none(),
+                "On initial epoch, secret-manager must be empty"
+            )
+        } else {
+            store.insert(oprf_key_id, OprfKeyMaterial::new(share, public_key, epoch));
+        }
+        Ok(())
+    }
+
+    pub async fn load_address(&self) -> eyre::Result<Address> {
+        Ok(self.wallet_private_key.address())
+    }
+
+    pub async fn get_oprf_key_material(
+        &self,
+        oprf_key_id: OprfKeyId,
+        epoch: ShareEpoch,
+    ) -> eyre::Result<Option<OprfKeyMaterial>> {
+        let key_material_epoch = self.store.lock().get(&oprf_key_id).cloned();
+        if let Some(key_material) = key_material_epoch
+            && key_material.is_epoch(epoch)
+        {
+            Ok(Some(key_material))
+        } else {
+            Ok(None)
+        }
     }
 }
