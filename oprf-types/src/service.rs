49cf68f1ddcd0c2c1a36f43d7425dc0553c01634
0xThemis <118167989+0xThemis@users.noreply.github.com>
2026-05-26 14:42:59 +0200

    feat(types): added node information to types

---
+++ oprf-types/src/service.rs
@@ -0,0 +1,82 @@
+//! Types for communication between key-gen and nodes.
+use std::num::NonZeroU16;
+
+use alloy::primitives::Address;
+use sqlx::{Row, postgres::PgRow};
+
+use crate::crypto::PartyId;
+
+/// All information necessary for an OPRF node provided by the key-gen instance.
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct NodeInformation {
+    party_id: PartyId,
+    address: Address,
+    threshold: NonZeroU16,
+}
+
+impl<'r> sqlx::FromRow<'r, PgRow> for NodeInformation {
+    fn from_row(row: &'r PgRow) -> Result<Self, sqlx::Error> {
+        let party_id: i32 = row.try_get("party_id")?;
+        let address: String = row.try_get("eth_address")?;
+        let threshold: i32 = row.try_get("threshold")?;
+        let Ok(party_id) = u16::try_from(party_id) else {
+            return Err(sqlx::Error::ColumnDecode {
+                index: "party_id".to_owned(),
+                source: "party_id does not fit into u16".into(),
+            });
+        };
+        let Ok(address) = Address::parse_checksummed(address, None) else {
+            return Err(sqlx::Error::ColumnDecode {
+                index: "eth_address".to_owned(),
+                source: "invalid address stored in DB".into(),
+            });
+        };
+        let Ok(threshold_u16) = u16::try_from(threshold) else {
+            return Err(sqlx::Error::ColumnDecode {
+                index: "threshold".to_owned(),
+                source: format!("expects non-zero u16 threshold, but got {threshold}").into(),
+            });
+        };
+        let Some(threshold) = NonZeroU16::new(threshold_u16) else {
+            return Err(sqlx::Error::ColumnDecode {
+                index: "threshold".to_owned(),
+                source: format!("expects non-zero threshold, but got {threshold}").into(),
+            });
+        };
+        Ok(Self {
+            party_id: PartyId(party_id),
+            address,
+            threshold,
+        })
+    }
+}
+
+impl NodeInformation {
+    /// Creates a new instance.
+    #[must_use]
+    pub fn new(party_id: PartyId, address: Address, threshold: NonZeroU16) -> Self {
+        Self {
+            party_id,
+            address,
+            threshold,
+        }
+    }
+
+    /// The [`PartyId`] of the node operator.
+    #[must_use]
+    pub fn party_id(&self) -> PartyId {
+        self.party_id
+    }
+
+    /// The EVM address of the node operator.
+    #[must_use]
+    pub fn address(&self) -> Address {
+        self.address
+    }
+
+    /// The threshold for the OPRF MPC instance.
+    #[must_use]
+    pub fn threshold(&self) -> NonZeroU16 {
+        self.threshold
+    }
+}
